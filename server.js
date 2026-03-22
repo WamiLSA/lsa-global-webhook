136cdcc56fe33e7e7b36b18a7acbd40afae9e08f
WamiLSA <contacts@lsa-global.com>
2026-03-22 23:12:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "LSA_GLOBAL_TOKEN";
+
+// VERIFY
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+// RECEIVE
 const axios = require("axios");
 
 const PHONE_NUMBER_ID = "YOUR_PHONE_NUMBER_ID";
@@ -39,3 +60,5 @@ app.post("/webhook", async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+app.listen(process.env.PORT || 10000, () => console.log("Server running"));
